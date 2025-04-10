7316b7a3d6a02c02db7ba47785aeb0096a90737b
FatimaMubeenSh <shfatimamubeen@gmail.com>
2025-04-10 16:22:35 +0500

    🎨 UI for sidebar and Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,7 +1,13 @@
+import logo from '../../assets/logo.png';
+
 const Header = () => {
   return (
-    <header className="bg-white shadow p-4 sticky top-0 z-10">
-      <h1 className="text-xl font-semibold">FocusFlow</h1>
+    <header className="bg-white shadow p-4 sticky top-0 z-10 flex">
+      <img src={logo} alt="FocusFlow Logo" className="h-9 mr-2" />
+      <h1 className="text-2xl font-bold">
+        <span>Focus</span>
+        <span className="text-purple-500">Flow</span>
+      </h1>
     </header>
   );
 }
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,13 +1,25 @@
 import { Link } from 'react-router-dom';
+import HomeIcon from '../../assets/icons/home.svg';
+import TimerIcon from '../../assets/icons/timer.svg';
+import TasksIcon from '../../assets/icons/tasks.svg';
+import GoalsIcon from '../../assets/icons/goal.svg';
 
 const Sidebar = () => {
   return (
-    <div className="w-25 bg-gray-800 text-white p-4">
-      <nav className="flex flex-col gap-4 mt-12">
-        <Link to="/">Home</Link>
-        <Link to="/pomodoro">Pomodoro</Link>
-        <Link to="/tasks">Tasks</Link>
-        <Link to="/goals">Goals</Link>
+    <div className="w-18 bg-gray-800 text-white p-4">
+      <nav className="flex flex-col gap-10 mt-16">
+        <Link to="/">
+          <img src={HomeIcon} alt="Home" className="w-10" />
+        </Link>
+        <Link to="/pomodoro">
+          <img src={TimerIcon} alt="Pomodoro" className="w-10" />
+        </Link>
+        <Link to="/tasks">
+          <img src={TasksIcon} alt="Tasks" className="w-10" />
+        </Link>
+        <Link to="/goals">
+          <img src={GoalsIcon} alt="Goals" className="w-10" />
+        </Link>
       </nav>
     </div>
   );
