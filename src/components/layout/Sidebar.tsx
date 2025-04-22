c0de0eff3db1bca403598edcb4d0864d13253949
FatimaMubeenSh <shfatimamubeen@gmail.com>
2025-04-22 15:12:52 +0500

    🐛 FIX: Refactor Sidebar links for improved structure and readability

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -8,18 +8,22 @@ const Sidebar = () => {
   return (
     <div className="shrink-0 w-18 md:w-18 lg:w-16 bg-gray-800 text-white p-4">
       <nav className="flex flex-col items-center gap-10 mt-16">
-        <Link to="/">
-          <img src={HomeIcon} alt="Home" className="w-20 md:w-10" />
-        </Link>
-        <Link to="/pomodoro">
-          <img src={TimerIcon} alt="Pomodoro" className="w-20 md:w-10" />
-        </Link>
-        <Link to="/tasks">
-          <img src={TasksIcon} alt="Tasks" className="w-20 md:w-10" />
-        </Link>
-        <Link to="/goals">
-          <img src={GoalsIcon} alt="Goals" className="w-20 md:w-10" />
-        </Link>
+      <Link to="/" className="flex flex-col items-center">
+        <img src={HomeIcon} alt="Home" className="w-20 md:w-10" />
+        <span className="mt-2 text-xs">Home</span>
+      </Link>
+      <Link to="/pomodoro" className="flex flex-col items-center">
+        <img src={TimerIcon} alt="Pomodoro" className="w-20 md:w-10" />
+        <span className="mt-2 text-xs">Timer</span>
+      </Link>
+      <Link to="/tasks" className="flex flex-col items-center">
+        <img src={TasksIcon} alt="Tasks" className="w-20 md:w-10" />
+        <span className="mt-2 text-xs">Tasks</span>
+      </Link>
+      <Link to="/goals" className="flex flex-col items-center">
+        <img src={GoalsIcon} alt="Goals" className="w-20 md:w-10" />
+        <span className="mt-2 text-xs">Goals</span>
+      </Link>
       </nav>
     </div>
   );
