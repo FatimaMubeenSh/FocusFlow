cf9aa02f67710da174fdb65f22d014eccc5b04e9
FatimaMubeenSh <shfatimamubeen@gmail.com>
2025-04-22 15:18:49 +0500

    👌 IMPROVE: Make Header Text and logo Responsive

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -24,8 +24,8 @@ const Header = () => {
   return (
     <header className="bg-white shadow p-4 sticky top-0 z-10 flex items-center justify-between">
       <div className="flex items-center gap-2">
-        <img src={logo} alt="FocusFlow Logo" className="h-9" />
-        <h1 className="text-2xl font-bold">
+        <img src={logo} alt="FocusFlow Logo" className="h-6 md:h-9" />
+        <h1 className="text-xl md:text-2xl font-bold">
           <span>Focus</span>
           <span className="text-purple-500">Flow</span>
         </h1>
